bf6676961b3eede405ccd6dcaf0de2814cca2e8e
MariaMushtaq-12 <maria.m.hashmi333@gmail.com>
2026-02-02 11:19:39 +0100

    merge of config is done

--- frontend/assets/js/core/config.js
+++ frontend/assets/js/core/config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-// config.js
-// Detect environment: localhost vs live
-const CONFIG = {
-  API_URL: window.location.hostname === "localhost"
-    ? "http://localhost:8020"           // your local backend
-    : "https://dqsurvey.onrender.com"  // live backend
-};
-=======
 export const CONFIG = {
   API_URL: window.location.hostname === "localhost"
     ? "http://localhost:8020"
@@ -16,4 +7,3 @@ export const CONFIG = {
 // export const CONFIG = {
 //   API_URL: "https://dqsurvey.onrender.com"
 // };
->>>>>>> 4e9388e4f6b80b04c55edba31a3be3cfd441644c
